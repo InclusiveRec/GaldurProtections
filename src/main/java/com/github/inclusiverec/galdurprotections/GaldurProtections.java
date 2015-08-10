69a2c0e9dfbeb0b5ad494c2377e971c5c4b55a61
usevalue <jurassic_pollard@hotmail.com>
2015-08-10 13:18:14 -0400

    Package created...

---
+++ src/main/java/com/github/inclusiverec/galdurprotections/GaldurProtections.java
@@ -0,0 +1,16 @@
+package com.github.inclusiverec.galdurprotections;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GaldurProtections extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        return;
+    }
+
+    @Override
+    public void onDisable() {
+        return;
+    }
+
+}
